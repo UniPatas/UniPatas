893a9971201753f1aa20fd75802d8226e702c4a8
pedroutopia <pedrocavalcanti.it@gmail.com>
2023-11-08 08:55:39 -0300

    Ainda estou vendo a melhor forma de criar as rotas, não finalizei

---
+++ src/app/login/layout.jsx
@@ -0,0 +1,12 @@
+export default function LoginLayout() {
+  return (
+    <>
+      <html>
+        <body>
+          <div>vou morrer</div>
+          <h1>morri ja</h1>
+        </body>
+      </html>
+    </>
+  );
+}
