3404c51c97d3b7eff889c456ced7aad6469159f1
Melferss <mayarafr09@hotmail.com>
2023-11-08 21:18:27 -0300

    Mantida as alterações da head

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,15 +2,8 @@ import  './globals.css'
 
 export default function Home() {
   return (
-<<<<<<< HEAD:src/app/page.jsx
-    <></>
-=======
 
-    <div>
-         <h1></h1>
-    </div>
-    
-       
->>>>>>> LP02-130:src/app/page.js
+  <></>
+
   )
 }
