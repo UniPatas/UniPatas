57cf031f185ea014ec2114a59a4cfcaf8b6232ea
pedroutopia <pedrocavalcanti.it@gmail.com>
2023-11-09 10:28:10 -0300

    Ajeitei as pastas do footer

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Anybody } from "next/font/google";
 import "./globals.css";
-import { Footer } from "@/components/pagHome/Footer/Footer";
+import { Footer } from "@/components/pagHome/Footer";
 
 const anybody = Anybody({
   subsets: ["latin"],
