08095e519d5288ecf18ab2f3a9ea8ade55d27c82
Melferss <mayarafr09@hotmail.com>
2023-11-11 00:27:54 -0300

    Componente Cadastro foi criado, layout.jsx e page.jsx foram criados.

---
+++ src/app/cadastro/layout.jsx
@@ -0,0 +1,13 @@
+import Cadastro from "@/components/Home/Cadastro/Cadastro";
+
+export default function CadastroLayout(){
+    return(
+        <>
+        <html>
+            <div>
+                <Cadastro/>
+            </div>
+        </html>
+        </>
+    );
+}
---
+++ src/components/Home/Cadastro/Cadastro.jsx
@@ -0,0 +1,20 @@
+"use client";
+import Capa from "@assets/capa.png";
+import Image from "next/image";
+import Logo from "@assets/logo-unipatas.png";
+import Nome from "@assets/logo-roxa-texto";
+
+export function Cadastro(){
+    return(
+        <>
+        <div className="bg-[#a110a9] w-screen h-screen flex items-center justify-center p-16 align-center">
+            <head>
+                <title>
+                    Cadastro | UniPatas
+                </title>
+            </head>
+
+        </div>
+        </>
+    )
+}
