7d731c7e4dd4bd2cd1422726905f4d677d8fa40b
Melferss <mayarafr09@hotmail.com>
2023-11-11 18:27:47 -0300

    Adicionado o checkbox no arquivo cadastro.jsx

--- src/components/Home/Cadastro/Cadastro.jsx
+++ src/components/Home/Cadastro/Cadastro.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import Nome from "@/assets/logo-roxa-texto.png";
 import Capa from "@/assets/capa.png";
@@ -14,7 +15,7 @@ export default function Cadastro(){
                 </title>
             </head>
             <div className=" w-96 h-full p-18 rounded-l-lg">
-            <div className="flex flex-col items-center pt-8 bg-white">
+            <div className="flex flex-col items-center pt-8 bg-white rounded-l-lg ">
             {" "}
             {/* conteiner da frase de boas vindas*/}
             <p class="bg-[#ffc501] text-[#a110a9] px-4 py-4 flex justify-center w-64 font-bold text-3xl rounded-full ">
@@ -37,28 +38,42 @@ export default function Cadastro(){
         </div>
         <div class="h-full w-96 bg-white rounded-r-lg flex flex-col p-8 ">
           {" "}
+
           {/*conteiner da logo */}
-          <div class="flex justify-center pt-4">
-            <Image src={Logo} width={120} height={50} alt="logo-unipatas" />
+          <div class="flex justify-center pt">
+            <Image src={Logo} width={100} height={50} alt="logo-unipatas" />
           </div>
+
+          <div>
+           <h2 className="flex justify-center font-bold text-xl">Cadastro</h2> 
+           <h2 className="flex justify-center font-bold text-sm" >Que tipo de perfil deseja?</h2>
+          </div>
+
           <form class="p-6 pr-6 flex jutify-center flex-col font-bold">
             {" "}
             {/*formulario do login */}
             <label> CPF </label>
-            <input type="email" class="bg-gray-200 h-8 rounded-md " />
-            <br />
+            <input type="email" class="bg-gray-200 h-8 rounded-md p-2 " />
+            
             <label> Email </label>
-            <input type="email" class="bg-gray-200 h-8 rounded-md" />
-            <br/>
+            <input type="email" class="bg-gray-200 h-8 rounded-md p-2" />
+        
             <label> Número telefone </label>
-            <input type="num" class="bg-gray-200 h-8 rounded-md " />
-            <br />
+            <input type="num" class="bg-gray-200 h-8 rounded-md p-2" />
+            
             <label> Login </label>
-            <input type="text" class="bg-gray-200 h-8 rounded-md " />
-            <br />
+            <input type="text" class="bg-gray-200 h-8 rounded-md p-2 " />
+            
             <label> Senha </label>
-            <input class="bg-gray-200 h-8 rounded-md " />
+            <input type="password" class="bg-gray-200 h-8 rounded-md p-2 " />
             <br />
+
+            <div className="text-xs"> 
+              <input type="checkbox" name="aceitarTermos" id="aceitarTermos" />
+              <label htmlFor="aceitarTermos"> Permito exibir minhas opções de contato no perfil</label>   
+            </div>
+           
+           
           </form>
 
             </div>
