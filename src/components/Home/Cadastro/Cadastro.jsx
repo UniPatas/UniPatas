6de889d37ac99831f2f5de61900cf456150f7771
Melferss <mayarafr09@hotmail.com>
2023-11-17 02:55:27 -0300

    Modificação na estrutura no arquivo Cadastro.jsx

--- src/components/Home/Cadastro/Cadastro.jsx
+++ src/components/Home/Cadastro/Cadastro.jsx
@@ -11,34 +11,29 @@ export default function Cadastro() {
 
     return (
         <>
-            <section className="bg-[#a110a9] align-center h-screen flex items-center justify-center">
-                <div className="  w-[768px] flex teste">
+            <section className="bg-[#a110a9] align-center h-screen flex items-center justify-center ">
+                <div className=" w-[768px] h-[676px] flex">
 
-                    <div className=" w-96 h-full p-18 rounded-l-lg h-auto">
-                        <div className="flex flex-col items-center pt-8 bg-white rounded-l-lg ">
+                    <div className="flex flex-col w-[384px] ">
+                        <Image
+                            src={Capa}
+                            alt="Cachorro amarelo de porte médio"
+                            className="rounded-l-lg h-[676px]"
+                        />
+                        <div className="fixed bottom-[430px] right-[820px]">
+                            <h1 className="bg-[#ffc501] text-[#a110a9] px-4 py-4 flex justify-center w- font-bold text-3xl rounded-full">Faça parte da</h1>
 
-                            {/* conteiner da frase 'faça parte da'*/}
-                            <p className="bg-[#ffc501] text-[#a110a9] px-4 py-4 flex justify-center w- font-bold text-3xl rounded-full ">
-                                Faça parte da
-                            </p>
                             <Image
                                 src={Nome}
-                                width={306}
-                                height={100}
-                                alt="text-unipatas"
-                                class="pt-2"
-                            />
-                            <Image
-                                src={Capa}
-                                width={337}
-                                alt="Cachorro amarelo de porte médio"
-                                className="h-auto"
+                                alt="Nome'Unipatas'"
+                                className="w-[280px] h-[100px]"
                             />
                         </div>
                     </div>
-                    <div class="h-full w-96 bg-white rounded-r-lg flex flex-col p-8 ">
 
-                        <div class="flex justify-center pt"> {/*conteiner da logo */}
+                    <div className="bg-white rounded-r-lg items-center flex flex-col w-[384px] h-[676px] gap-1 ">
+
+                        <div className="flex justify-center pt-2"> {/*conteiner da logo */}
                             <Image src={Logo} width={100} height={50} alt="logo-unipatas" />
                         </div>
 
@@ -47,29 +42,30 @@ export default function Cadastro() {
                             <h2 className="flex justify-center font-bold text-sm" >Que tipo de perfil deseja?</h2>
                         </div>
 
-                        <div className="flex justify-center space-x-4 pt-2">
+                        <div className="flex justify-center space-x-4 gap-px">
                             <Link href={"#"} className="bg-[#a110a9] h-8 w-18 rounded-full px-4 pt-2 font-bold text-xs text-center text-white">Usuario</Link>
                             <Link href={"/ong"} className="bg-[#33b3a6] h-8 w-18 rounded-full px-4 pt-2 font-bold text-xs text-center text-white">ONG</Link>
                         </div>
 
-                        <form class="p-2 pr-6 flex jutify-center flex-col font-bold"> {/*formulario do cadastro */}
+                        <form className=" flex jutify-center flex-col font-bold w-72 gap-1"> {/*formulario do cadastro */}
 
                             <label> CPF </label>
-                            <input type="email" class="bg-gray-200 h-8 rounded-md p-2 " />
+                            <input type="email" className="bg-gray-200 h-8 rounded-md " />
 
                             <label> Email </label>
-                            <input type="email" class="bg-gray-200 h-8 rounded-md p-2" />
+                            <input type="email" className="bg-gray-200 h-8 rounded-md p-2" />
 
                             <label> Número telefone </label>
-                            <input type="num" class="bg-gray-200 h-8 rounded-md p-2" />
+                            <input type="num" className="bg-gray-200 h-8 rounded-md p-2" />
 
                             <label> Login </label>
-                            <input type="text" class="bg-gray-200 h-8 rounded-md p-2 " />
+                            <input type="text" className="bg-gray-200 h-8 rounded-md p-2 " />
 
                             <label> Senha </label>
-                            <input type="password" class="bg-gray-200 h-8 rounded-md p-2 " />
+                            <input type="password" className="bg-gray-200 h-8 rounded-md p-2 " />
+                            <br />
 
-                            <div className="text-xs pt-1">  {/*Conteiner checkbox */}
+                            <div className="text-xs">  {/*Conteiner checkbox */}
                                 <input type="checkbox" name="aceitarTermos" id="aceitarTermos" />
                                 <label htmlFor="aceitarTermos"> Permito exibir minhas opções de contato no perfil</label>
                                 <br />
@@ -77,8 +73,9 @@ export default function Cadastro() {
                                 <label htmlFor="#">Lar temporário/voluntário</label>
                             </div>
                         </form>
+                        <br />
 
-                        <div className="flex space-x-20">
+                        <div className="flex space-x-20 ">
                             <Link href={"/login"}> Voltar </Link>
                             <button className="bg-[#a110a9] h-8 w-24 rounded-md text-white font-bold text-xs "> Cadraste-se </button>
                         </div>
