87ced74f7c3a33f61386ac5131e163d7d4d60f7d
Melferss <mayarafr09@hotmail.com>
2023-11-17 17:09:09 -0300

    Ajuste de width e height para correção de erros no Login.jsx

--- src/components/Home/Login/Login.jsx
+++ src/components/Home/Login/Login.jsx
@@ -11,83 +11,79 @@ export default function Login() {
 
   return (
     <>
-      <div className=" bg-[#a110a9] w-screen h-screen flex items-center justify-center p-16 align-center ">
+      <section className=" bg-[#a110a9] h-screen flex items-center justify-center align-center ">
+        <div className="w-[768px] h-[676px] flex">
 
-        <div className=" w-96 h-full p-18 rounded-l-lg">
-          {/* conteiner do mascote*/}
-
-          <div className="flex flex-col items-center pt-8 bg-white rounded-l-lg ">
-
-            {/* conteiner da frase de boas vindas*/}
-            <p className="bg-[#ffc501] text-[#a110a9] px-4 py-4 flex justify-center w-64 font-bold text-3xl rounded-full ">
-              Bem vindos a
-            </p>
-            <Image
-              src={Nome}
-              width={306}
-              height={100}
-              alt="text-unipatas"
-              class="pt-2"
-            />
+          <div className="flex flex-col w-[384px] ">
             <Image
               src={Capa}
-              width={337}
-              height={100}
               alt="Cachorro amarelo de porte médio"
+              className="rounded-l-lg h-[676px]"
             />
+            <div className="fixed bottom-[430px] right-[820px]">
+              <h1 className="bg-[#ffc501] text-[#a110a9] px-4 py-4 flex justify-center w- font-bold text-3xl rounded-full">Bem vindos a </h1>
+
+              <Image
+                src={Nome}
+                alt="Nome'Unipatas'"
+                className="w-[280px] h-[100px]"
+              />
+            </div>
           </div>
-        </div>
-        <div className="h-full w-96 bg-white rounded-r-lg flex flex-col p-8 ">
-          
-          {/*conteiner da logo */}
-          <div className="flex justify-center pt-4">
-            <Image src={Logo} width={120} height={50} alt="logo-unipatas" />
-          </div>
-          <form className="p-6 pr-6 flex jutify-center flex-col font-bold">
+
+          <div className="bg-white rounded-r-lg items-center flex flex-col w-[384px] h-[676px] gap-6 bottom-9] ">
+
+            {/*conteiner da logo */}
+            <div className="flex justify-center pt-20 ">
+              <Image src={Logo} width={120} height={50} alt="logo-unipatas" />
+            </div>
+
+            <div className="gap-28">
+            <form className=" flex jutify-center flex-col font-bold w-72 gap-2"> {/*formulario do login */}
+              <label> Login </label>
+              <input type="email" className="bg-gray-200 h-8 rounded-md " />
+              <br/>
+              <label> Senha </label>
+              <input type="password" className="bg-gray-200 h-8 rounded-md" />
+            </form>
+            </div>
+
+            <div className="flex justify-around space-x-20"> {/* conteiner dos botões*/}
+              <button className="bg-[#a110a9] h-6 w-16 px-2 rounded-md text-white font-bold text-xs">
+                Entrar
+              </button>
+
+              <Link href="/cadastro" className="bg-gray-200 h-6 w-18 rounded-md px-2 pt-1 font-bold text-xs">
+                Cadastre-se
+              </Link>
+
+            </div>
+            <p className="flex justify-center items-center text-xs font-bold">
+              Esqueceu a senha?
+            </p>
             
-            {/*formulario do login */}
-            <label> Login </label>
-            <input type="email" className="bg-gray-200 h-8 rounded-md p-1" />
-            <br />
-            <label> Senha </label>
-            <input type="password" className="bg-gray-200 h-8 rounded-md p-1" />
-          </form>
 
-          <div className="flex justify-around "> {/* conteiner dos botões*/}
-            <button className="bg-[#a110a9] h-6 w-16 px-2 rounded-md text-white font-bold text-xs">
-              Entrar
-            </button>
+            <div className="flex justify-center text-xs"> {/* conteiner google-facebook*/}
 
-            <Link href="/cadastro" className="bg-gray-200 h-6 w-18 rounded-md px-2 pt-1 font-bold text-xs">
-              Cadastre-se
-            </Link>
-
-          </div>
-          <p className="pt-4 flex justify-center items-center text-xs font-bold">
-            Esqueceu a senha?
-          </p>
-          <br />
-
-          <div className="flex justify-center text-xs"> {/* conteiner google-facebook*/}
-
-            <Image src={Google} width={20} height={50} alt="logo-google" />
-            <button>Entrar com o Google</button>
-          </div>
-          <br />
-          <div className="flex justify-center">
-            <Image
-              src={Facebook}
-              width={50}
-              height={10}
-              alt="logo-facebook"
-              className=" rounded-l-lg"
-            />
-            <button className="h-8 w-36 rounded-r-lg bg-[#2b78e4] text-white text-xs">
-              Entrar com o Facebook
-            </button>
+              <Image src={Google} width={20} height={50} alt="logo-google" />
+              <button>Entrar com o Google</button>
+            </div>
+            
+            <div className="flex justify-center">
+              <Image
+                src={Facebook}
+                width={50}
+                height={10}
+                alt="logo-facebook"
+                className=" rounded-l-lg"
+              />
+              <button className="h-8 w-36 rounded-r-lg bg-[#2b78e4] text-white text-xs">
+                Entrar com o Facebook
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
