84550096b18295dc115e8f7635ad427ae72adf24
pedroutopia <pedrocavalcanti.it@gmail.com>
2023-11-19 15:58:15 -0300

    Botão de criar novas publicações finalizado.

--- src/components/BotaoNovaPub.jsx
+++ src/components/BotaoNovaPub.jsx
@@ -7,15 +7,15 @@ export function BotaoNovaPub () {
     return (
         <>
             <section>
-                <button>
-                    <div className=" flex items-center justify-between bg-lighter-green p-[10px] rounded-[10px] border-[3px] border-darker-purple">
+                <button onClick={"função aqui"}>
+                    <div className=" flex items-center justify-between bg-pool-green p-[10px] rounded-[10px] border-[3px] border-ligther-green hover:bg-lighter-green hover:border-pool-green text-[20px] text-white font-black ml-[20px] hover:text-darker-purple">
                         <div>
                             <Image className=" w-[40px]"
                             src={ Plus }
                             ></Image>
                         </div>
-                        <div>
-                            <p className="text-[20px] text-white font-black ml-[20px]">Criar publicação</p>
+                        <div className=" ml-[10px]">
+                            <p>Criar publicação</p>
                         </div>
                     </div>
                 </button>
