680746104e084d888e1d5d410035c0cc63a8ebc9
thidspessoa <thiagopessoa246@gmail.com>
2023-11-20 14:16:07 -0300

    Criado icone arredondado de chat-bot flutuante

---
+++ src/components/Home/ChatBot/ChatBot.jsx
@@ -0,0 +1,28 @@
+import '../../../app/globals.css'
+
+export default function ChatBot () {
+    return (
+        <>
+            <section>
+
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+                <aside>
+
+                    <a href='#'>
+                        <div className="rounded-full bg-[#33b3a6] w-[80px] h-[80px] fixed right-[19px] flex">
+                            <span class="material-symbols-outlined teste">
+                                mark_unread_chat_alt
+                            </span>
+                        </div>
+                    </a>
+
+
+
+                </aside>
+
+            </section>
+        </>
+    )
+};
+
