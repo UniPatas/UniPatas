43cc77669657e60c02b235e42c240367cb6da4cd
Melferss <mayarafr09@hotmail.com>
2023-11-20 02:52:15 -0300

    Botão adicionado

--- src/components/Home/CadastroOngs/CadastroOngs.jsx
+++ src/components/Home/CadastroOngs/CadastroOngs.jsx
@@ -20,8 +20,8 @@ export default function CadastroOngs() {
                                 className="w-[350px] absolute inset-y-64 left-36 bottom-0" />
                         </div>
 
-                        <div className=" relative flex" >
-                            <h1 className="absolute inset-y-48 left-56 bottom-0 px-4 py-4 flex justify-center w-66 font-bold text-3xl rounded-full ">Bem vindos a</h1>
+                        <div className=" relative" >
+                            <p className=" bg-[#33b3a6] pb-12 text-center w-72 h-12 text-white absolute inset-y-48 left-44 bottom-0 w-66 font-bold text-3xl rounded-full ">Bem vindos a</p>
                         </div>
 
                         <div className="relative">
@@ -31,21 +31,34 @@ export default function CadastroOngs() {
                         </div>
                     </div>
 
-                    <div className="w-[642px] h-auto relative">
-                        <form className="flex flex-col gap-4 absolute inset-y-32 left-0 bottom-8 ">
-
-                            <div className="flex flex-wrap gap">
-                                <label className="" htmlFor="nomeOng">Nome da ong</label>
-                                <input className="bg-gray-200 h-8 rounded-md w-[188px]" type="text" id="nomeOng"></input>
-                                <label className="" htmlFor="nomeResponsavel">Nome do responsável</label>
-                                <input className="bg-gray-200 h-8 rounded-md w-[188px]" type="text" id="nomeResponsavel"></input>
-                            </div>
+                    <div className="w-[642px] h-auto relative ">
+                        <form className="w-[642px] absolute inset-y-44 left-0 bottom-8 space-y-6">
 
                             <div className="">
-                                <label className="block" htmlFor="numeroContato">Numero de contato</label>
+                                <label className="block" htmlFor="nomeOng">Nome da ong</label>
+                                <div>
+                                    <input className="bg-gray-200 h-8 rounded-md w-[300px]" type="text" id="nomeOng"></input>
+                                </div>
+                            </div>
+
+                            <div className="relative">
+                                <label className="absolute inset-x-96 right-12 bottom-[55px] " htmlFor="nomeResponsavel">Nome do responsável</label>
+                                <div className="relative">
+                                    <input className="bg-gray-200 h-8 w-[350px] rounded-md absolute inset-x-96 right-24 bottom-[22px] " type="text" id="nomeResponsavel"></input>
+                                </div>
+                            </div>
+
+
+                            <div className="">
+                                <label className="block" htmlFor="numeroContato">Número de contato</label>
                                 <input className="bg-gray-200 h-8 rounded-md w-[188px]" type="text" id="numeroContato"></input>
-                                <label className="block" htmlFor="cpfResponsavel">CPF do responsável</label>
-                                <input className="bg-gray-200 h-8 rounded-md w-[188px]" type="text" id="cpfResponsavel"></input>
+                            </div>
+
+                            <div className="relative">
+                                <label className="absolute inset-x-96 right-12 bottom-[59px]" htmlFor="cpfResponsavel">CPF do responsável</label>
+                                <div>
+                                    <input className="bg-gray-200 h-8 rounded-md w-[350px] absolute inset-x-96 right-24 bottom-[28px] " type="text" id="cpfResponsavel"></input>
+                                </div>
                             </div>
 
                             <div>
@@ -53,22 +66,33 @@ export default function CadastroOngs() {
                                 <input type="text" className="bg-gray-200 h-8 rounded-md w-[188px] " id="cep"></input>
                             </div>
 
-                            <div className="flex flex-wrap gap-x-4 ">
+                            <div className=" ">
                                 <label className="" htmlFor="cidade">Cidade</label>
-                                <input className="bg-gray-200 h-8 rounded-md w-[188px]" type="text" id="cidade"></input>
-                                <label className="" htmlFor="uf">UF</label>
-                                <input className="bg-gray-200 h-8 rounded-md w-[188px]" type="text" id="uf"></input>
+                                <div>
+                                    <input className="bg-gray-200 h-8 rounded-md w-[188px]" type="text" id="cidade"></input>
+                                </div>
                             </div>
+
+                            <div className="relativo">
+                                <label className="absolute inset-x-96 left-64 bottom-[345px]" htmlFor="uf">UF</label>
+                                <div>
+                                    <input className="bg-gray-200 h-8 rounded-md w-[40px] absolute inset-x-96 left-64 bottom-[312px]" type="text" id="uf"></input>
+                                </div>
+                            </div>
+
+
 
                             <div>
                                 <label className="block" htmlFor="endereço">Endereço</label>
-                                <input className="bg-gray-200 h-8 rounded-md w-[188px]" type="text" id="endereço"></input>
+                                <input className="bg-gray-200 h-8 rounded-md w-[300px]" type="text" id="endereço"></input>
                             </div>
 
-                            <div className=" text-sm font-bold">
+                            <div className=" text-sm font-bold space-x-8">
                                 <br />
                                 <a
                                     href="/cadastro"> Voltar para cadastro de usuário</a>
+
+                                <button className="bg-[#a110a9] h-6 w-24 rounded-md text-white font-bold text-xs  ">Proximo</button>
                             </div>
                         </form>
                         <div>
