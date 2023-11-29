3e254c838be7a335a020bfec0e36ccd0e5ce9547
pedroutopia <pedrocavalcanti.it@gmail.com>
2023-11-29 09:57:16 -0300

    Merge

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import { NavBar } from "@/components/Home/NavBar";
 import { Footer } from "./Home/Footer";
 import ChatBot from "./Home/ChatBot/ChatBot";
+
 export default function Layout({ children }) {
   return (
     <>
@@ -9,8 +10,7 @@ export default function Layout({ children }) {
       <ChatBot />
       <Footer />
       <div id="modal-root"></div>
-
-
+ 
     </>
   );
-} 
+}
