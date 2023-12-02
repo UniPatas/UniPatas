0761603cc37a971948f63d33a052bcb38d73f7d5
thidspessoa <thiagopessoa246@gmail.com>
2023-12-02 19:07:03 -0300

    Resolvido bug de duplicação do layout apos o merge

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -21,8 +21,6 @@ export default function RootLayout({ children }) {
         <CadastroContextProvider>
           {children}
         </CadastroContextProvider>
-        <></>
-        {children}
       </body>
     </html>
   );
