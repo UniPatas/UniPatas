4f9c9672183076e3f6fe0253c97a65e0a43020ee
pedroutopia <pedrocavalcanti.it@gmail.com>
2023-12-02 13:51:10 -0300

    Imagem final adicionada ao container imagem,
    também foi acrescentado um container para centralizar a imagem

--- src/app/doe/layout.jsx
+++ src/app/doe/layout.jsx
@@ -1,14 +1,14 @@
 import Layout from "@/components/Layout";
 import ContainerImgDoe from "@/components/Doe/ContainerImgDoe";
-import Exemplo from "@/assets/exemplo.png";
+import BannerDonation from "@/assets/banner_donation.png";
 import NavigateToHome from "@/components/Doe/NavigateToHome";
 
 export default function DoeLayout() {
   return (
     <>
       <Layout>
-        <ContainerImgDoe img={Exemplo} />
         <NavigateToHome tela="Doe" rota="doe" />
+        <ContainerImgDoe img={BannerDonation} />
       </Layout>
     </>
   );
--- src/components/Doe/ContainerImgDoe.jsx
+++ src/components/Doe/ContainerImgDoe.jsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 
-export default function ContainerImgDoe({img}) {
+export default function ContainerImgDoe({ img }) {
   return (
     <>
       <section className="py-[30px]">
         <div className="w-full mx-auto">
-          <div className="max-w-[1246px] mx-auto">
-            <div>
-                <Image className="h-[100%]"
-                src={img}
-                alt=""
-                />
+          <div className="max-w-[1246px] mx-auto ">
+            {/* Centralizar imagem */}
+            <div className=" flex justify-center">
+              <div className=" max-w-[1100px] ">
+                <Image className="h-[100%]" src={img} alt="" />
+              </div>
             </div>
           </div>
         </div>
