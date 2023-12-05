9dcf1a54072b1f5a05cf05cfa3a99f8cdcc99513
Melferss <mayarafr09@hotmail.com>
2023-12-05 15:58:50 -0300

    Mais dois cards incluidos.

--- src/components/Doe/ConteinerQrcode/Cards.jsx
+++ src/components/Doe/ConteinerQrcode/Cards.jsx
@@ -9,7 +9,7 @@ export default function Cards() {
     return (
 
         <>
-            <section className="">
+            <section className="w-[100%]">
                 <div className="space-y-16">{/*Div pai*/}
                     <div className=" flex items-center justify-center font-bold text-3xl">
                         <h1>DOAÇÃO</h1>
@@ -26,7 +26,7 @@ export default function Cards() {
                                 <Image
                                     src={Logo}
                                     alt="Logo roxa unipatas"
-                                    className="w-[75%] ml-4 self-start" />
+                                    className="w-[75%]  ml-4 self-start" />
 
                                 <Image
                                     src={Hast}
@@ -95,6 +95,50 @@ export default function Cards() {
 
                                 <Image
                                     src={Hast1}
+                                    alt="Hastg"
+                                    className="w-[75%] mt-4 ml-4" />
+
+                                <h1 className="text-[#ffc501] bg-[#a110a9] flex justify-center ml-4 mt-6 w-[130px] font-bold text-[10px] rounded-full">www.unipatas.com </h1>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="bg-white flex justify-center gap-24">{/*Conteiner filho*/}
+                        <div className="bg-[#33b3a6]  w-[20%] h-[200px] rounded-[20px] flex items-center">{/*conteiner do qrcode */}
+                            <Image
+                                src={Qr}
+                                alt="qrcode da ong"
+                                className="ml-4 w-[48%]" />
+
+                            <div className="">{/*conteiner com hastg e logo unipatas*/}
+                                <Image
+                                    src={Logo}
+                                    alt="Logo roxa unipatas"
+                                    className="w-[75%] ml-4 self-start" />
+
+                                <Image
+                                    src={Hast}
+                                    alt="Hastg"
+                                    className="w-[75%] mt-4 ml-4" />
+
+                                <h1 className="text-[#ffc501] bg-[#a110a9] flex justify-center ml-4 mt-6 w-[130px] font-bold text-[10px] rounded-full">www.unipatas.com </h1>
+                            </div>
+                        </div>
+                        <div className="bg-[#33b3a6]  w-[20%] h-[200px] rounded-[20px] flex items-center">{/*conteiner do qrcode */}
+                            <Image
+                                src={Qr}
+                                alt="qrcode da ong"
+                                className="ml-4 w-[48%]" />
+
+                            <div className="">{/*conteiner com hastg e logo unipatas*/}
+                                <Image
+                                    src={Logo}
+                                    alt="Logo roxa unipatas"
+                                    className="w-[75%] ml-4 self-start" />
+
+                                <Image
+                                    src={Hast}
                                     alt="Hastg"
                                     className="w-[75%] mt-4 ml-4" />
 
