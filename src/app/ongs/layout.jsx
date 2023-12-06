31d91110ffd967b1e889779d860bf3d10f899937
pedroutopia <pedrocavalcanti.it@gmail.com>
2023-12-06 12:17:38 -0300

    Conatiner de imagem ongs criado

--- src/app/ongs/layout.jsx
+++ src/app/ongs/layout.jsx
@@ -1,10 +1,12 @@
 import Layout from "@/components/Layout";
+import BannerOngs from "@/assets/banner-ongs.png";
+import ContainerImg from "@/components/Doe/ContainerImg";
 
 export default function OngsLayout() {
   return (
     <>
       <Layout>
-        <h1>Ong's</h1>
+        <ContainerImg img={BannerOngs} alt={"Banner de imagem Ong's"} />
       </Layout>
     </>
   );
