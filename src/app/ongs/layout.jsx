f67702c34f5ae976cdef4e77748172953e421291
pedroutopia <pedrocavalcanti.it@gmail.com>
2023-12-06 12:19:08 -0300

    Componente navigate criado

--- src/app/ongs/layout.jsx
+++ src/app/ongs/layout.jsx
@@ -1,11 +1,13 @@
 import Layout from "@/components/Layout";
 import BannerOngs from "@/assets/banner-ongs.png";
+import NavigateToHome from "@/components/Doe/NavigateToHome";
 import ContainerImg from "@/components/Doe/ContainerImg";
 
 export default function OngsLayout() {
   return (
     <>
       <Layout>
+        <NavigateToHome tela="Ongs" rota="ongs" />
         <ContainerImg img={BannerOngs} alt={"Banner de imagem Ong's"} />
       </Layout>
     </>
