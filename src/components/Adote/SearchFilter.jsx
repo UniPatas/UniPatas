585096fbdbf1b5595122ea91f902dcf58b0b485e
pedroutopia <pedrocavalcanti.it@gmail.com>
2023-12-05 22:00:10 -0300

    Squashed commit of the following:
    commit 4debfedf13e5c2bd4bb2524b9041e47b91a57a90
    Merge: 93e351a e44980d
    Author: pedroutopia <pedrocavalcanti.it@gmail.com>
    Date:   Tue Dec 5 21:59:10 2023 -0300
    
        Merge branch 'main' of https://github.com/pedroutopia/UniPatas
    
    commit e44980d72ff59a8a95547cca047797ebd5a2e1f9
    Author: Thiago Pessoa <104585635+thidspessoa@users.noreply.github.com>
    Date:   Tue Dec 5 19:41:05 2023 -0300
    
        LP03-266 | Criar componente visual do container de filtros e busca (Pagina de feed adote) (#55)
    
        * Construida toda a parte visual, falta finalizar algumas pendencias
    
        * Corrigido bug dos filtros estarem vazando no modal de novas publi, adicionei um z-index no modal
    
        * Finalizada logica de selecionar opção no menuDropDown e aparecer no campo do filtro
    
        * Finalizada logica de ativação dos menus DropDowns
    
    commit 673f29e561140b0b914db7bfe40181df5373dc78
    Merge: abfaaac 56f2a36
    Author: Maria Clara <142630780+mari-devv@users.noreply.github.com>
    Date:   Tue Dec 5 18:50:27 2023 -0300
    
        Merge pull request #54 from UniPatas/LP03-150_Cadastro_ong
    
        LP03-150 - Página de cadastro - Ong
    
    commit 56f2a36fd84bc174d10e6f984ea15183464ab62f
    Author: mariaclara <mariaclara8779@gmail.com>
    Date:   Tue Dec 5 18:47:03 2023 -0300
    
        LP03-150 - Criei um objeto para a ong, fica mais facil de lidar com os dados... api de cep e cadastro inclusos. Validações também
    
    commit 351ea4d97bb034721f970bf6485eb1b4c3afe3c0
    Author: mariaclara <mariaclara8779@gmail.com>
    Date:   Tue Dec 5 18:45:47 2023 -0300
    
        LP03-150 - Removido componentes extras, centralizei todo o conteudo na página do cadastro.
    
    commit bdb8c1e39be296a0f5afc95a544f8a634f1f1975
    Author: mariaclara <mariaclara8779@gmail.com>
    Date:   Tue Dec 5 18:45:10 2023 -0300
    
        LP03-150 - Incluido novamente o toastfy/react - biblioteca de alertas para tratar erros e sucessos

---
+++ src/components/Adote/SearchFilter.jsx
@@ -0,0 +1,249 @@
+"use client";
+
+import { useState } from "react";
+import '../../app/globals.css';
+import { handleClientScriptLoad } from "next/script";
+
+const SearchFilter = () => {
+    
+    //States dos menusDropDowns
+    const [menuDropEspecie, setMenuDropEspecie] = useState(false); 
+    const [menuDropSexo, setMenuDropSexo] = useState(false);
+    const [menuDropPorte, setMenuDropPorte] = useState(false);
+    const [menuDropPostagem, setMenuDropPostagem] = useState(false);
+
+    //States dos filtros
+    const [especieAnimal, setEspecieAnimal] = useState('Especie do animal'); 
+    const [sexoAnimal, setSexoAnimal] = useState('Sexo do animal');
+    const [porteAnimal, setPorteAnimal] = useState('Porte do animal');
+    const [tipoPostagem, setTipoPostagem] = useState('Tipo da postagem');
+
+
+    const toggleMenu = (evento) => { {/*Função que atualiza o estado do menu, que é usado para determinar se será exibido ou não */}
+        const { id } = evento.target; //Captura o name do input que registrou o evento, e o seu valor
+
+        const listId = { //Objeto que armazena cada id de cada input, e uma função para armazenar dentro do input o valor que vem pelo contexto
+            'menuDropEspecie': function () {
+                if (menuDropEspecie) { //Condicional usada para controlar a abertura e fechamento do menu de acordo com o estate do mesmo.
+                    setMenuDropEspecie(false);
+                } else {
+                    setMenuDropEspecie(true);
+                }
+            },
+            'menuDropSexo': function () {
+                if (menuDropSexo) { //Condicional usada para controlar a abertura e fechamento do menu de acordo com o estate do mesmo.
+                    setMenuDropSexo(false);
+                } else {
+                    setMenuDropSexo(true);
+                }
+            },
+            'menuDropPorte': function () {
+                if (menuDropPorte) { //Condicional usada para controlar a abertura e fechamento do menu de acordo com o estate do mesmo.
+                    setMenuDropPorte(false);
+                } else {
+                    setMenuDropPorte(true);
+                }
+            },
+            'menuDropPostagem': function () {
+                if (menuDropPostagem) { //Condicional usada para controlar a abertura e fechamento do menu de acordo com o estate do mesmo.
+                    setMenuDropPostagem(false);
+                } else {
+                    setMenuDropPostagem(true);
+                }
+            }
+        };
+
+        for (let key in listId) { //Itera sobre cada propiedade do objeto listId
+            if (id === key) { //Verifica se o id do elemeto que ativou o evento, é igual a alguma das propiedades do listId
+                const element = listId[id](); //Caso seja, vai chamar o set que possui dentro de cada propiedade do objeto
+            }
+        };
+    };
+
+    const handleOptionSelection = (evento) => { {/*Opçao que seta o valor padrão exibido em cada botão de cada menu dropDown quando o usuario seleciona uma opção */}
+        const { id } = evento.target; //Captura o name do input que registrou o evento, e o seu valor
+
+        const elemento = document.getElementById(id).textContent;
+
+        const listId = { //Objeto que armazena cada id de cada input, e uma função para armazenar dentro do input o valor que vem pelo contexto
+            'cachorro': function () {
+                setEspecieAnimal(elemento)
+            }, 
+            'gato': function () {
+                setEspecieAnimal(elemento);
+            },
+            'todos': function () {
+                setEspecieAnimal(elemento);
+            },
+            'macho': function () {
+                setSexoAnimal(elemento);
+            },
+            'femea': function () {
+                setSexoAnimal(elemento);
+            },
+            'todosSexos': function() {
+                setSexoAnimal(elemento);
+            },
+            'grande': function() {
+                setPorteAnimal(elemento);
+            },
+            'medio': function() {
+                setPorteAnimal(elemento);
+            },
+            'pequeno': function() {
+                setPorteAnimal(elemento);
+            },
+            'todosTamanhos': function() {
+                setPorteAnimal(elemento);
+            },
+            'maisRecente': function() {
+                setTipoPostagem(elemento);
+            },
+            'deOngs': function() {
+                setTipoPostagem(elemento);
+            },
+            'deUsuario': function() {
+                setTipoPostagem(elemento);
+            },
+            'maisAntigas': function() {
+                setTipoPostagem(elemento);
+            },
+            'todasPostagens': function() {
+                setTipoPostagem(elemento)
+            }
+        };
+    
+        for (let key in listId) { //Itera sobre cada propiedade do objeto listId
+            if (id === key) { //Verifica se o id do elemeto que ativou o evento, é igual a alguma das propiedades do listId
+                const element = listId[id](); //Caso seja, vai chamar o set que possui dentro de cada propiedade do objeto
+            }
+        };
+    };
+
+
+    return (
+        <>
+            <section className="max-w-[1246px] mx-auto bg-[#a110a9] flex justify-center"> {/*Filter section */}
+                <div className="w-auto h-[326px] space-y-5 flex items-center"> {/*Body-Filter */}
+
+                    <div className="space-y-8"> {/*Container Filters-Engloba todo conteudo do body*/}
+
+                        <div className="grid grid-cols-3 gap-4 space-x-8"> {/*1x3 linhas e colunas */}
+
+                            <div className="relative"> {/*Filtro de especie de animal */}
+                                <button id="menuDropEspecie" className="bg-white border border-gray-300 focus:border-blue-500 rounded px-3 py-1 text-lg text-gray-600
+                                placeholder-gray-300 focus:outline-none shadow flex gap-2 w-[250px] h-[50px] justify-center font-black items-center	" onClick={toggleMenu}
+                                title="Especie de Animais"> {/*Botão que registra o click Do usuario */}
+                                    {especieAnimal} {/*Gera condicionalmente o texto padrão dentro do botão que agrupa o dropDown*/}
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"> {/*Icone de seta que indica um menu dropDown */}
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                    </svg>
+                                </button>
+                                <div className={`inline bg-white border border-gray-300 py-1 shadow-md rounded-md  w-[250px] h-auto absolute text-lg ${menuDropEspecie ? '' : 'hidden'}`}> {/*Menu dropDown da especie dos animais e suas opções(cada link) */}
+                                    {/*Na div acima, utilizei uma renderização condicional para a classe hidden, ela só vai existir caso o estado do "menuDrop" seja falso, ou seja, so vai esconder
+                                    o menuDropDown caso ele não esteja visivel */}
+                                    <button onClick={handleOptionSelection} name="cachorro" id="cachorro"  className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">Cachorro</button>
+                                    <button onClick={handleOptionSelection} name="gato" id="gato" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">Gato</button>
+                                    <button onClick={handleOptionSelection} name="todos" id="todos" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">Todos</button>
+
+                                </div>
+                            </div>
+
+                            <div className="relative"> {/*Filtro de sexo do animal */}
+                                <button id="menuDropSexo" className="bg-white border border-gray-300 focus:border-blue-500 rounded px-3 py-1 text-lg text-gray-600
+                                placeholder-gray-300 focus:outline-none shadow flex items-center gap-2 w-[250px] h-[50px] justify-center font-black	" onClick={toggleMenu}> {/*Botão que registra o click Do usuario */}
+                                    {sexoAnimal}
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"> {/*Icone de seta que indica um menu dropDown */}
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                    </svg>
+                                </button>
+                                <div className={`inline bg-white border border-gray-300 py-1 shadow-md rounded-md  w-[250px] h-auto absolute text-lg ${menuDropSexo ? '' : 'hidden'}`}> {/*Menu dropDown da especie dos animais e suas opções(cada link) */}
+                                    {/*Na div acima, utilizei uma renderização condicional para a classe hidden, ela só vai existir caso o estado do "menuDrop" seja falso, ou seja, so vai esconder
+                                    o menuDropDown caso ele não esteja visivel */}
+                                    <button onClick={handleOptionSelection} name="macho" id="macho" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">Macho</button>
+                                    <button onClick={handleOptionSelection} name="femea" id="femea" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">Fêmea</button>
+                                    <button onClick={handleOptionSelection} name="todosSexos" id="todosSexos" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">Todos os sexos</button>
+
+                                </div>
+                            </div>
+
+                            <div className="relative"> {/*Filtro do porte do animal */}
+                                <button id="menuDropPorte" className="bg-white border border-gray-300 focus:border-blue-500 rounded px-3 py-1 text-lg text-gray-600
+                                placeholder-gray-300 focus:outline-none shadow flex items-center gap-2 w-[250px] h-[50px] justify-center font-black	" onClick={toggleMenu}> {/*Botão que registra o click Do usuario */}
+                                    {porteAnimal}
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"> {/*Icone de seta que indica um menu dropDown */}
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                    </svg>
+                                </button>
+                                <div className={`inline bg-white border border-gray-300 py-1 shadow-md rounded-md  w-[250px] h-auto absolute text-lg ${menuDropPorte ? '' : 'hidden'}`}> {/*Menu dropDown da especie dos animais e suas opções(cada link) */}
+                                    {/*Na div acima, utilizei uma renderização condicional para a classe hidden, ela só vai existir caso o estado do "menuDrop" seja falso, ou seja, so vai esconder
+                                    o menuDropDown caso ele não esteja visivel */}
+                                    <button onClick={handleOptionSelection} name="grande" id="grande" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">Grande</button>
+                                    <button onClick={handleOptionSelection} name="medio" id="medio" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">Medio</button>
+                                    <button onClick={handleOptionSelection} name="pequeno" id="pequeno" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">Pequeno</button>
+                                    <button onClick={handleOptionSelection} name="todosTamanhos" id="todosTamanhos" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">Todos os tamanhos</button>
+
+
+                                </div>
+                            </div>
+
+                        </div> {/*1x3 linhas e colunas */}
+
+
+                        <div className="w-full flex justify-center items-center"> {/*Container que agrupa a search bar e o filtro de postagem */}
+                            <div className="grid grid-cols-3 gap-4  w-auto space-x-5"> {/*1x2 linhas e colunas */}
+
+                                <div className="col-start-1 col-span-2 "> {/*Filtro de nome do animal */}
+                                    <input className="bg-white border border-gray-300 focus:border-blue-500 rounded px-3 py-1 text-lg text-gray-600
+                                    placeholder-gray-300 focus:outline-none shadow flex items-center gap-2 w-full h-[50px] justify-center font-black" type="text" placeholder="Nome do animal..." /> {/*Botão que registra o click Do usuario */}
+                                </div>
+
+                                <div className="col-end-4"> {/*Filtro do tempo/tipo da postagem */}
+                                    <button id="menuDropPostagem" className="bg-white border border-gray-300 focus:border-blue-500 rounded px-3 py-1 text-lg text-gray-600
+                                    placeholder-gray-300 focus:outline-none shadow flex items-center gap-2 w-[250px] h-[50px] justify-center font-black" onClick={toggleMenu}> {/*Botão que registra o click Do usuario */}
+                                        {tipoPostagem}
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5"> {/*Icone de seta que indica um menu dropDown */}
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+                                    </button>
+                                    <div className={`inline bg-white border border-gray-300 py-1 shadow-md rounded-md w-[250px] h-auto absolute text-lg ${menuDropPostagem ? '' : 'hidden'}`}> {/*Menu dropDown da especie dos animais e suas opções(cada link) */}
+                                        {/*Na div acima, utilizei uma renderização condicional para a classe hidden, ela só vai existir caso o estado do "menuDrop" seja falso, ou seja, so vai esconder
+                                        o menuDropDown caso ele não esteja visivel */}
+                                        <button onClick={handleOptionSelection} name="maisRecente" id="maisRecente" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">Mais Recente</button>
+                                        <button onClick={handleOptionSelection} name="deOngs" id="deOngs" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">De Ongs</button>
+                                        <button onClick={handleOptionSelection} name="deUsuario" id="deUsuario" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">De Usuários</button>
+                                        <button onClick={handleOptionSelection} name="maisAntigas" id="maisAntigas" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">Mais Antigas</button>
+                                        <button onClick={handleOptionSelection} name="todasPostagens" id="todasPostagens" className="block w-full text-sm text-gray-500 px-3 py-1 bg-white hover:bg-gray-300">Todas as Postagens</button>
+
+                                    </div>
+                                </div>
+
+                            </div> {/*1x2 linhas e colunas */}
+
+                        </div>
+
+
+
+
+                        <div className="flex justify-center">
+
+                            <div className="relative"> {/*Botão de buscar*/}
+                                <button className="bg-[#33b3a6] border border-[#33b3a6] focus:border-blue-500 rounded px-3 py-1 text-lg text-white
+                                placeholder-gray-300 focus:outline-none shadow flex items-center gap-2 w-[250px] h-[50px] justify-center font-black"> {/*Botão que registra o click Do usuario */}
+                                    Buscar
+                                </button>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+
+                </div> {/*Body-Filter */}
+            </section>
+        </>
+    )
+};
+
+export default SearchFilter;
