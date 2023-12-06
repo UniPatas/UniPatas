bbe29b9fc8cd679b48765c43afa997a5023cc06e
Melferss <mayarafr09@hotmail.com>
2023-12-06 16:04:53 -0300

    Estrutura do conteiner do parceiros e apadrinhamento finalizados.

--- src/app/doe/layout.jsx
+++ src/app/doe/layout.jsx
@@ -3,6 +3,7 @@ import ContainerImgDoe from "@/components/Doe/ContainerImg";
 import BannerDonation from "@/assets/banner_donation.png";
 import NavigateToHome from "@/components/Doe/NavigateToHome";
 import Cards from "@/components/Doe/ConteinerQrcode/Cards";
+import Paceiros from "@/components/Doe/ConteinerParceiros/EmpresaParceiras";
 
 export default function DoeLayout() {
   return (
@@ -11,6 +12,7 @@ export default function DoeLayout() {
         <NavigateToHome tela="Doe" rota="doe" />
         <ContainerImgDoe img={BannerDonation} alt={"Banner de imagem Doe"} />
         <Cards/>
+        <Paceiros/>
       </Layout>
     </>
   );
