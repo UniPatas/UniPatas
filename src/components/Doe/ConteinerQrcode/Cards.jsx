54f128357c37a07cdd97ef5ba92fbb543c7b58d1
Melferss <mayarafr09@hotmail.com>
2023-12-09 14:28:44 -0300

    Bug no componente cardQR resolvido

--- src/components/Doe/ConteinerQrcode/Cards.jsx
+++ src/components/Doe/ConteinerQrcode/Cards.jsx
@@ -8,115 +8,87 @@ export default function Cards() {
 
         <>
             <section className="w-[100%]">
-                <div className="space-y-16">{/*Div pai*/}
-                    <div className=" flex items-center justify-center font-bold text-3xl">
-                        <h1>DOAÇÃO</h1>
-                    </div>
+                <div className="space-y-16 flex justify-center">{/*Div pai*/}
+                    <div className="space-y-16 w-[850px] h-[550px] flex flex-col justify-center">
+                        <h1 className="flex items-top justify-center font-bold text-3xl">DOAÇÃO</h1>
+                        <div className="bg-white flex justify-center gap-24 w-[100%] h-[200px]"> {/*Div filho*/}
+                       
+                            <div className="bg-[#33b3a6] rounded-[20px] flex items-center">{/*conteiner do qrcode */} 
+                            <div className="flex w-[375px]">
+                                <Image
+                                    src={Qr}
+                                    alt="qrcode da ong"
+                                    className="ml-4 w-[40%] " />
 
-                    <div className="bg-white flex justify-center gap-24 "> {/*Div filho*/}
-                        <div className="bg-[#33b3a6]  w-[380px] h-[200px] rounded-[20px] flex items-center">{/*conteiner do qrcode */}
-                            <Image
-                                src={Filo}
-                                alt="qrcode da ong"
-                                className="ml-4 w-[40%] " />
+                                <div className="">{/*conteiner com hastg e logo unipatas*/}
+                                    <Image
+                                        src={Logo}
+                                        alt="Logo roxa unipatas"
+                                        className="w-[75%] ml-4 self-start" />
 
-                            <div className="">{/*conteiner com hastg e logo unipatas*/}
-                                <h1 className="text-brown font-bold m-2 text-2xl">Cantinho da Filó</h1>
-                                <h1 className="bg-[#33b3a6] text-white m-4 ">@cantinhodafilo</h1>
-                                <h1 className="text-[#ffc501] bg-[#a110a9] flex justify-center ml-4 mt-6 w-[130px] font-bold text-[10px] rounded-full">cantinhodafilo.com.br</h1>
+                                    <h1 className="bg-[#33ba6] text-white m-4 ">@unipatas.plataforma</h1>
+
+                                    <h1 className="text-[#ffc501] bg-[#a110a9] flex justify-center ml-4 mt-6 w-[130px] font-bold text-[10px] rounded-full">www.unipatas.com </h1>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="bg-[#33b3a6] rounded-[20px] flex items-center">{/*conteiner do qrcode */}
+                            <div className="flex w-[375px]">
+                                <Image
+                                    src={Qr}
+                                    alt="qrcode da ong"
+                                    className="ml-4 w-[40%]" />
+
+                                <div className="">{/*conteiner com hastg e logo unipatas*/}
+                                    <Image
+                                        src={Logo}
+                                        alt="Logo roxa unipatas"
+                                        className="w-[75%] ml-4 self-start" />
+
+                                    <h1 className="bg-[#33ba6] text-white m-4 ">@unipatas.plataforma</h1>
+
+                                    <h1 className="text-[#ffc501] bg-[#a110a9] flex justify-center ml-4 mt-6 w-[130px] font-bold text-[10px] rounded-full">www.unipatas.com </h1>
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
-                        <div className="bg-[#33b3a6]  w-[380px] h-[200px] rounded-[20px] flex items-center">{/*conteiner do qrcode */}
-                            <Image
-                                src={Qr}
-                                alt="qrcode da ong"
-                                className="ml-4 w-[40%]" />
-
-                            <div className="">{/*conteiner com hastg e logo unipatas*/}
+                        <div className="bg-white flex justify-center gap-24 w-[100%] h-[200px]"> {/*Div filho*/}
+                            <div className="bg-[#ffc501] rounded-[20px] flex items-center">{/*conteiner do qrcode */}
                                 <Image
-                                    src={Logo}
-                                    alt="Logo roxa unipatas"
-                                    className="w-[75%] ml-4 self-start" />
+                                    src={Qr}
+                                    alt="qrcode da ong"
+                                    className="ml-4 w-[40%]" />
 
-                                <h1 className="bg-[#33ba6] text-white m-4 ">@unipatas.plataforma</h1>
+                                <div className="">{/*conteiner com hastg e logo unipatas*/}
+                                    <Image
+                                        src={Logo}
+                                        alt="Logo roxa unipatas"
+                                        className="w-[75%] ml-4 self-start" />
 
-                                <h1 className="text-[#ffc501] bg-[#a110a9] flex justify-center ml-4 mt-6 w-[130px] font-bold text-[10px] rounded-full">www.unipatas.com </h1>
+                                    <h1 className="bg-[#ffc501] text-white m-4">@unipatas.plataforma</h1>
+
+                                    <h1 className="text-[#ffc501] bg-[#a110a9] flex justify-center ml-4 mt-6 w-[130px] font-bold text-[10px] rounded-full">www.unipatas.com </h1>
+                                </div>
                             </div>
-                        </div>
-                    </div>
 
-                    <div className="bg-white flex justify-center gap-24"> {/*Div filho*/}
-                        <div className="bg-[#ffc501]  w-[380px] h-[200px] rounded-[20px] flex items-center">{/*conteiner do qrcode */}
-                            <Image
-                                src={Qr}
-                                alt="qrcode da ong"
-                                className="ml-4 w-[40%]" />
-
-                            <div className="">{/*conteiner com hastg e logo unipatas*/}
+                            <div className="bg-[#ffc501] rounded-[20px] flex items-center">{/*conteiner do qrcode */}
                                 <Image
-                                    src={Logo}
-                                    alt="Logo roxa unipatas"
-                                    className="w-[75%] ml-4 self-start" />
+                                    src={Qr}
+                                    alt="qrcode da ong"
+                                    className="ml-4 w-[40%]" />
 
-                                <h1 className="bg-[#ffc501] text-white m-4">@unipatas.plataforma</h1>
+                                <div className="">{/*conteiner com hastg e logo unipatas*/}
+                                    <Image
+                                        src={Logo}
+                                        alt="Logo roxa unipatas"
+                                        className="w-[75%] ml-4 self-start" />
 
-                                <h1 className="text-[#ffc501] bg-[#a110a9] flex justify-center ml-4 mt-6 w-[130px] font-bold text-[10px] rounded-full">www.unipatas.com </h1>
-                            </div>
-                        </div>
+                                    <h1 className="bg-[#ffc501] text-white m-4">@unipatas.plataforma</h1>
 
-                        <div className="bg-[#ffc501]  w-[380px] h-[200px] rounded-[20px] flex items-center">{/*conteiner do qrcode */}
-                            <Image
-                                src={Qr}
-                                alt="qrcode da ong"
-                                className="ml-4 w-[40%]" />
-
-                            <div className="">{/*conteiner com hastg e logo unipatas*/}
-                                <Image
-                                    src={Logo}
-                                    alt="Logo roxa unipatas"
-                                    className="w-[75%] ml-4 self-start" />
-
-                                <h1 className="bg-[#ffc501] text-white m-4">@unipatas.plataforma</h1>
-
-                                <h1 className="text-[#ffc501] bg-[#a110a9] flex justify-center ml-4 mt-6 w-[130px] font-bold text-[10px] rounded-full">www.unipatas.com </h1>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="bg-white flex justify-center gap-24">{/*Conteiner filho*/}
-                        <div className="bg-[#33b3a6]  w-[380px] h-[200px] rounded-[20px] flex items-center">{/*conteiner do qrcode */}
-                            <Image
-                                src={Qr}
-                                alt="qrcode da ong"
-                                className="ml-4 w-[40%]" />
-
-                            <div className="">{/*conteiner com hastg e logo unipatas*/}
-                                <Image
-                                    src={Logo}
-                                    alt="Logo roxa unipatas"
-                                    className="w-[75%] ml-4 self-start" />
-
-                                <h1 className="bg-[#33b3a6] text-white m-4">@unipatas.plataforma</h1>
-
-                                <h1 className="text-[#ffc501] bg-[#a110a9] flex justify-center ml-4 mt-6 w-[130px] font-bold text-[10px] rounded-full">www.unipatas.com </h1>
-                            </div>
-                        </div>
-                        <div className="bg-[#33b3a6]  w-[380px] h-[200px] rounded-[20px] flex items-center">{/*conteiner do qrcode */}
-                            <Image
-                                src={Qr}
-                                alt="qrcode da ong"
-                                className="ml-4 w-[40%]" />
-
-                            <div className="">{/*conteiner com hastg e logo unipatas*/}
-                                <Image
-                                    src={Logo}
-                                    alt="Logo roxa unipatas"
-                                    className="w-[70%] ml-4 self-start" />
-
-                                <h1 className="bg-[#33b3a6] text-white m-4">@unipatas.plataforma</h1>
-
-                                <h1 className="text-[#ffc501] bg-[#a110a9] flex justify-center ml-4 mt-6 w-[130px] font-bold text-[10px] rounded-full">www.unipatas.com </h1>
+                                    <h1 className="text-[#ffc501] bg-[#a110a9] flex justify-center ml-4 mt-6 w-[130px] font-bold text-[10px] rounded-full">www.unipatas.com </h1>
+                                </div>
                             </div>
                         </div>
                     </div>
