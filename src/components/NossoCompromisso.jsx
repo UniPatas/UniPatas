cdb41b87a6f16005a01872dfd986713e228c98a6
pedroutopia <pedrocavalcanti.it@gmail.com>
2023-12-10 02:38:27 -0300

    Iniciando componente nosso compromisso social

---
+++ src/components/NossoCompromisso.jsx
@@ -0,0 +1,12 @@
+export default function NossoCompromisso() {
+  return (
+    <>
+      <section>
+        <div className=" w-full max-w-[1246px] px-[50px] mx-auto">
+            {/* Tittulo */}
+            <h1 className=" text-center text-[30px] text-darker-purple">Nosso Compromisso social</h1>
+        </div>
+      </section>
+    </>
+  );
+}
