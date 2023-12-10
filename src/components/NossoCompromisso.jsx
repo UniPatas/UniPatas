60a9c939ac9572d9b3595e02c710af61a9f1523f
pedroutopia <pedrocavalcanti.it@gmail.com>
2023-12-10 02:46:38 -0300

    Componente finalizado

--- src/components/NossoCompromisso.jsx
+++ src/components/NossoCompromisso.jsx
@@ -2,9 +2,21 @@ export default function NossoCompromisso() {
   return (
     <>
       <section>
-        <div className=" w-full max-w-[1246px] px-[50px] mx-auto">
-            {/* Tittulo */}
-            <h1 className=" text-center text-[30px] text-darker-purple">Nosso Compromisso social</h1>
+        <div className=" w-full max-w-[1246px] mx-auto">
+          {/* Tittulo */}
+          <h1 className=" text-center text-[30px] font-bold text-darker-purple py-[20px]">
+            Nosso Compromisso social
+          </h1>
+          <div className=" flex justify-center items-center">
+          <p className=" w-[65%] text-justify">
+            A missão da UniPatas é promover a conexão genuína entre humanos e
+            animais, construindo pontes de compaixão e amor. Buscamos criar uma
+            plataforma inclusiva e inovadora, onde a tecnologia e o cuidado se
+            entrelaçam para transformar vidas, fortalecendo a comunidade que
+            compartilha o compromisso de proteger e valorizar todas as formas de
+            vida.
+          </p>
+          </div>
         </div>
       </section>
     </>
