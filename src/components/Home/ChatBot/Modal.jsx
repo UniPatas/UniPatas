0bb8fcb3247fcab0bf3cf0ae68c0983a04206575
thidspessoa <thiagopessoa246@gmail.com>
2023-12-10 19:27:35 -0300

    Adicionado efeito de mensagem sendo renderizada enquanto mensagem do chatBot é carregada

--- src/components/Home/ChatBot/Modal.jsx
+++ src/components/Home/ChatBot/Modal.jsx
@@ -9,6 +9,8 @@ const Modal = ({ onClose, children }) => {
   const [userMessage, setUserMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const messagesContainerRef = useRef();
+  const [isLoading, setIsLoading] = useState(false); //estado para controlar o estado de carregamento  da resposta do chatBot
+
 
   const handleCloseClick = (e) => { //Função para fechar o modal
     e.preventDefault();
@@ -28,8 +30,11 @@ const Modal = ({ onClose, children }) => {
       setMessages([...messages, { text: userMessage, user: true }]);
       setUserMessage('');
 
+      //Ativa o estado de carregamento
+      setIsLoading(true);
+
       // Simula um atraso de 1 segundo antes de obter a resposta do chatBot
-      await new Promise(resolve => setTimeout(resolve, 1000));
+      await new Promise(resolve => setTimeout(resolve, 2000));
 
       const res = await fetch('http://localhost:5000/post_response', {
         method: 'POST',
@@ -46,9 +51,13 @@ const Modal = ({ onClose, children }) => {
         ...prevMessages,
         { text: data.response, user: false },
       ]);
-      
+
     } catch (error) {
       console.error('Erro ao enviar mensagem:', error);
+    } finally {
+      
+      // Desativa o estado de carregamento, independentemente do resultado
+      setIsLoading(false);
     }
   };
 
@@ -119,6 +128,25 @@ const Modal = ({ onClose, children }) => {
 
               </div>
             ))}
+
+            {isLoading && (
+              <div class="border border-blue-300 shadow rounded-r-lg  rounded-b-lg  p-4 max-w-sm w-[70%] mb-[3rem] mx-auto">
+                <div class="animate-pulse flex space-x-4">
+                  <div class="rounded-full bg-slate-200 h-10 w-10"></div>
+                  <div class="flex-1 space-y-6 py-1">
+                    <div class="h-2 bg-slate-200 rounded"></div>
+                    <div class="space-y-3">
+                      <div class="grid grid-cols-3 gap-4">
+                        <div class="h-2 bg-slate-200 rounded col-span-2"></div>
+                        <div class="h-2 bg-slate-200 rounded col-span-1"></div>
+                      </div>
+                      <div class="h-2 bg-slate-200 rounded"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )} {/*Exibe esse container de conteudo sendo carregado, caso a mensagem do chatBot não tenha sido enviada ainda */}
+
           </div>
           <div className="flex border-t border-t-black self-center rounded-b-lg w-[400px] h-[100px]"> {/*Area de digitar*/}
             <input
