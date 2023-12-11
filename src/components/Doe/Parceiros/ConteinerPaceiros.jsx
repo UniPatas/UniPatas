8f3eb92800d7630e159f7f2dabbabcce952cd0f7
pedroutopia <pedrocavalcanti.it@gmail.com>
2023-12-10 22:33:51 -0300

    Squashed commit of the following:
    commit e587a1b3bc48fa7f72aaf6d478d4583c25bd3d6c
    Merge: 02ed177 a205a1f
    Author: pedroutopia <pedrocavalcanti.it@gmail.com>
    Date:   Sun Dec 10 22:33:26 2023 -0300
    
        Merge branch 'main' of https://github.com/pedroutopia/UniPatas
    
    commit a205a1f6cde9d5187dbd11685f853ca0b7657639
    Merge: 1d79f50 da96f18
    Author: Mayara Melo <142054842+Melferss@users.noreply.github.com>
    Date:   Sun Dec 10 22:31:06 2023 -0300
    
        Merge pull request #82 from Melferss/LP03-409
    
        LP03-409 Textos adicionados na tela doe
    
    commit 02ed17724cbe42b59d945a24fa6834e998c2bf4f
    Merge: 68f67ef 1d79f50
    Author: pedroutopia <pedrocavalcanti.it@gmail.com>
    Date:   Sun Dec 10 22:27:09 2023 -0300
    
        Merge branch 'main' of https://github.com/pedroutopia/UniPatas
    
    commit da96f18e26d2e260bd6573d8a7d8eed33b6e068d
    Author: Melferss <mayarafr09@hotmail.com>
    Date:   Sun Dec 10 22:24:01 2023 -0300
    
        Textos adicionados na tela doe
    
    commit 68f67ef87bf27a0a04c67b0ee79742240f3c9352
    Merge: 863b168 f978604
    Author: pedroutopia <pedrocavalcanti.it@gmail.com>
    Date:   Sun Dec 10 02:50:47 2023 -0300
    
        Merge branch 'main' of https://github.com/pedroutopia/UniPatas
    
    commit 863b168c8f07333749dc7f52cd36e4c35817c90a
    Merge: 90a47ad de8bd08
    Author: pedroutopia <pedrocavalcanti.it@gmail.com>
    Date:   Sun Dec 10 02:26:17 2023 -0300
    
        Merge branch 'main' of https://github.com/pedroutopia/UniPatas

--- src/components/Doe/Parceiros/ConteinerPaceiros.jsx
+++ src/components/Doe/Parceiros/ConteinerPaceiros.jsx
@@ -11,22 +11,20 @@ export default function Paceiros() {
                                 APADRINHE UMA CAUSA
                             </h1>
                             <p className="indent-12 text-xl text-slate-600 font-bold tracking-wide">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptate,
-                                eligendi earum sunt natus laboriosam animi dolorem perspiciatis aliquam, et cum.
-                                Nemo at quae exercitationem ut aspernatur, repellat neque possimus!
+                            Seja o herói que um amigo peludo espera encontrar! Ao apadrinhar um animal na UniPatas, você não apenas oferece um lar temporário amoroso, mas também cria uma conexão única que transforma vidas.
+                             Torne-se um padrinho ou madrinha e faça parte dessa jornada de amor e cuidado.
                             </p>
-                            <div className="flex justify-center"><Link href={"/"} className="flex bg-[#ffc501] text-[#a110a9] font-bold w-[250px] h-[30px] justify-center text-[18px]">Entre em contato</Link></div>
+                            <div className="flex justify-center"><Link href={"/"} className="flex bg-[#ffc501] text-[#a110a9] font-bold w-[250px] h-[30px] justify-center text-[18px] rounded-md">Entre em contato</Link></div>
                         </div>
                         <div className="w-[650px] space-y-12">
-                            <h1 className="text-3xl font-bold text-center ">
+                            <h1 className="text-3xl font-bold text-center">
                                 SEJA UMA EMPRESA PARCEIRA
                             </h1>
                             <p className="indent-12 text-xl text-slate-600 font-bold tracking-wide">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium voluptate,
-                                eligendi earum sunt natus laboriosam animi dolorem perspiciatis aliquam, et cum.
-                                Nemo at quae exercitationem ut aspernatur, repellat neque possimus!
+                            Junte-se a nós como uma empresa parceira comprometida com o bem-estar animal! Ao unir forças com a UniPatas, sua empresa não apenas fortalece sua imagem socialmente responsável, mas também faz a diferença real na vida de animais necessitados.
+                             Seja uma voz para os sem voz e promova a adoção responsável. Juntos, podemos criar um impacto duradouro!
                             </p>
-                            <div className="flex justify-center "><Link href={"/"} className="flex bg-[#ffc501] text-[#a110a9] font-bold w-[250px] h-[30px] justify-center text-[18px] mb-12">Entre em contato</Link></div>
+                            <div className="flex justify-center "><Link href={"/"} className="flex bg-[#ffc501] text-[#a110a9] font-bold w-[250px] h-[30px] justify-center text-[18px] mb-12 rounded-md">Entre em contato</Link></div>
                         </div>
                     </div>
                 </div>
