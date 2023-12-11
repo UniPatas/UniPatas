00541ef4f174dbd036ed6d7bb109485febac2be7
pedroutopia <pedrocavalcanti.it@gmail.com>
2023-12-11 00:25:25 -0300

    Imagem mudada e link adicionado ao patriocinador

--- src/components/Home/ContainerAnuncio.jsx
+++ src/components/Home/ContainerAnuncio.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import ImgAnuncio from "@/assets/anuncie-aqui.png";
+import ImgAnuncio from "@/assets/bannerdonation.png";
 
 export function ContainerAnuncio() {
   return (
@@ -7,7 +7,7 @@ export function ContainerAnuncio() {
     {/* Componente de anuncio de parceiro */}
       <section>
         <div className=" w-full max-w-[1246px] px-[50px] mx-auto">
-          <a href="#" target="_blank">
+          <a href="https://www.petz.com.br/" target="_blank">
             <Image className="py-[30px]" src={ImgAnuncio} alt="Anúncio." />
           </a>
         </div>
